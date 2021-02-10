2ae2efe52ebb658a70a732ad26f114ba011b136d
zzs <270901260@qq.com>
2021-02-11 07:57:22 +0800

    first commit

---
+++ src/main/java/com/business/chinese/ChineseApplication.java
@@ -0,0 +1,13 @@
+package com.business.chinese;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChineseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChineseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/business/chinese/ChineseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.business.chinese;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChineseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
